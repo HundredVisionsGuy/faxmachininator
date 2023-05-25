4bc2b13c21726d9ca7ed35e5efd294b8082ff0df
hundredvisionsguy <winikkc@hsd.k12.or.us>
2023-05-25 10:06:29 -0700

    adding more functions

--- faxmachininator/app.py
+++ faxmachininator/app.py
@@ -14,9 +14,17 @@ def main():
     algorithm = get_file_contents("data/", "sample.txt")
 
     # draw picture
-    print(algorithm)
+    dimension = get_dimensions(algorithm)
+    print(dimension)
 
-def get_file_contents(path: str, filename: str) -> str:
+def get_dimensions(algorithm: str) -> tuple:
+    """Get the full algorithm text and return the width and height
+    as a tuple"""
+
+    return get_dimensions(algorithm)
+
+
+def get_file_contents(path: str, filename: str) -> list:
     """Returns the contents of the file in the path.
 
     Args:
@@ -28,8 +36,10 @@ def get_file_contents(path: str, filename: str) -> str:
 
     folder = Path(path)
     file_to_open = folder / filename
-    f = open(file_to_open)
-    return f.read()
+    f = open(file_to_open, 'r')
+    rows = f.readlines()
+    f.close()
+    return rows
 
 if __name__ == "__main__":
     main()
